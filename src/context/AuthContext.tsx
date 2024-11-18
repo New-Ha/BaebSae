63399d2118065c4f9177663e5f7a8afbb3b0bac5
UHa <https00200@gmail.com>
2024-11-18 20:14:14 +0900

    🚀 [Feat]: Modify also deliver fireStore's users collection in AuthContext
    - AuthContext에서 fireStore의 users collection도 함께 제공하도록 수정
    - firebase Auth는 인증에만 사용하기로 함

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,30 +1,51 @@
+import { userDocumentRef } from 'constants/refs';
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import { onSnapshot } from 'firebase/firestore';
 import { app } from 'firebaseApp';
 import { createContext, ReactNode, useEffect, useState } from 'react';
 
+export interface UserType {
+    uid: string;
+    email: string;
+    displayName: string;
+    photoURL: string;
+    [key: string]: any;
+}
+
+interface AuthContextType {
+    isAuthenticated: boolean;
+    user: UserType | null;
+}
 interface AuthProps {
     children: ReactNode;
 }
 
-const AuthContext = createContext({
-    user: null as User | null,
+const AuthContext = createContext<AuthContextType>({
+    isAuthenticated: false,
+    user: null,
 });
 
 export const AuthContextProvider = ({ children }: AuthProps) => {
-    const [currentUser, setCurrentUser] = useState<User | null>(null);
+    const [authUser, setAuthUser] = useState<User | null>(null);
+    const [user, setUser] = useState<UserType | null>(null);
+
     const auth = getAuth(app);
 
     useEffect(() => {
-        onAuthStateChanged(auth, user => {
-            if (user) {
-                setCurrentUser(user);
-            } else {
-                setCurrentUser(null);
-            }
+        onAuthStateChanged(auth, curUser => {
+            setAuthUser(curUser);
         });
     }, [auth]);
 
-    return <AuthContext.Provider value={{ user: currentUser }}>{children}</AuthContext.Provider>;
+    useEffect(() => {
+        if (authUser) {
+            onSnapshot(userDocumentRef(authUser.uid), snapshot => {
+                setUser((snapshot.data() as UserType) || null);
+            });
+        }
+    }, [authUser]);
+
+    return <AuthContext.Provider value={{ isAuthenticated: !!authUser, user }}>{children}</AuthContext.Provider>;
 };
 
 export default AuthContext;
