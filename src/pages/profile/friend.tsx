eed7f0c95a026344a8cea14d5473f0b2ef5a2f3e
UHa <https00200@gmail.com>
2024-11-18 22:14:42 +0900

    🚑 [Fix]: Modify to reflect friend collection in real time
    - friend page에서도 fiend 여부를 수정할 수 있도록 real time으로 반영되도록 수정

--- src/pages/profile/friend.tsx
+++ src/pages/profile/friend.tsx
@@ -1,22 +1,39 @@
-import NoPostBox from 'components/posts/NoPostBox';
-import UserBox from 'components/sidebar/UserBox';
-import { friendDocumentRef } from 'constants/refs';
-import AuthContext from 'context/AuthContext';
-import { getDoc } from 'firebase/firestore';
 import { useContext, useEffect, useState } from 'react';
+import AuthContext from 'context/AuthContext';
+import { onSnapshot } from 'firebase/firestore';
+import { friendDocumentRef } from 'constants/refs';
+import UserBox from 'components/sidebar/UserBox';
+import NoPostBox from 'components/posts/NoPostBox';
 
 export default function MyFriendPage() {
     const { user } = useContext(AuthContext);
     const [friends, setFriends] = useState<string[]>([]);
 
     useEffect(() => {
-        (async () => {
-            const friendSnap = await getDoc(friendDocumentRef(user?.uid as string));
-            if (friendSnap.exists()) {
-                setFriends((friendSnap.data() as string[]) || []);
+        if (!user?.uid) return;
+
+        onSnapshot(friendDocumentRef(user?.uid as string), snapshot => {
+            const data = snapshot.data();
+            if (data?.users) {
+                const friendIds = data.users.map((user: { id: string }) => user.id);
+                setFriends(friendIds);
+            } else {
+                setFriends([]);
             }
-        })();
+        });
     }, [user?.uid]);
 
-    return <>{friends.length > 0 ? <UserBox /> : <NoPostBox />}</>;
+    return (
+        <div>
+            {friends.length > 0 ? (
+                friends.map(friendUid => (
+                    <div key={friendUid}>
+                        <UserBox userUid={friendUid} />
+                    </div>
+                ))
+            ) : (
+                <NoPostBox />
+            )}
+        </div>
+    );
 }
