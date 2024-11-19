a0c9e693c498e09b8c7a326d638772943f40b72f
UHa <https00200@gmail.com>
2024-11-19 09:36:13 +0900

    🚀 [Feat]: Add popular page
    - 좋아요 수가 많은 게시글을 보여주는 페이지

--- src/pages/popular/index.tsx
+++ src/pages/popular/index.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { PostType } from 'pages/home';
+import { onSnapshot, orderBy, query } from 'firebase/firestore';
+import { postListCollectionRef } from 'constants/refs';
+import Header from 'components/common/Header';
+import PostBox from 'components/posts/PostBox';
 
 export default function PopularPage() {
-	return (
-		<div>PopularPage</div>
-	)
+    const [posts, setPosts] = useState<PostType[]>([]);
+
+    useEffect(() => {
+        const popularQuery = query(postListCollectionRef, orderBy('likesCount', 'desc'), orderBy('createdAt', 'desc'));
+        onSnapshot(popularQuery, snapshot => {
+            const dataObj = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setPosts(dataObj as PostType[]);
+        });
+    }, [posts]);
+
+    return (
+        <>
+            <Header title="Popular" />
+            {posts.map(post => (
+                <PostBox key={post.id} post={post} />
+            ))}
+        </>
+    );
 }
