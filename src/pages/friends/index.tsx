c68ea4ded938eb3e17b5f5743cd7cad0b6712945
UHa <https00200@gmail.com>
2024-11-30 19:38:59 +0900

    🚀 [Feat]: Add friend pages
    - 추가한 친구들 목록을 가져와 friendIds에 저장
    - friendIds에 있는 user들이 작성한 게시글을 가져와 최신순으로 보여줌
    - 친구들이 작성한 게시글이 없을 때와, 등록한 친구가 없는 때를 나누어 NoContentBox 메시지를 달리함

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -1,5 +1,61 @@
-import React from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
+import AuthContext from 'context/AuthContext';
+import { onSnapshot, orderBy, query, where } from 'firebase/firestore';
+import { friendDocumentRef, postListCollectionRef } from 'constants/refs';
+import { PostType } from 'pages/home';
+import PostBox from 'components/posts/PostBox';
+import NoContentBox from 'components/posts/NoContentBox';
+import { useNavigate } from 'react-router-dom';
 
 export default function FriendsPostPage() {
-    return <div>FriendsPostPage</div>;
+    const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
+    const [friendIds, setFriendIds] = useState<string[]>([]);
+    const [friendPosts, setFriendPosts] = useState<PostType[]>([]);
+
+    useEffect(() => {
+        if (!user?.uid) return;
+
+        onSnapshot(friendDocumentRef(user?.uid as string), snapshot => {
+            const friendIds = snapshot.data()?.users.map((user: { id: string }) => user.id || []);
+            setFriendIds(friendIds);
+        });
+    }, [user?.uid]);
+
+    useEffect(() => {
+        if (friendIds.length !== 0) {
+            (async () => {
+                const postsQuery = query(
+                    postListCollectionRef,
+                    where('uid', 'in', friendIds),
+                    orderBy('createdAt', 'desc'),
+                );
+
+                onSnapshot(postsQuery, snapshot => {
+                    let dataObj = snapshot.docs.map(doc => ({
+                        ...doc.data(),
+                        id: doc.id,
+                    }));
+
+                    setFriendPosts(dataObj as PostType[]);
+                });
+            })();
+        }
+    }, [friendIds]);
+
+    return (
+        <div className="home__post_list">
+            {friendPosts.length !== 0 ? (
+                friendPosts.map(post => (
+                    <div key={post.id} className="box__hover" onClick={() => navigate(`/posts/${post.id}`)}>
+                        <PostBox post={post} />
+                    </div>
+                ))
+            ) : friendIds.length !== 0 ? (
+                <NoContentBox text="친구들이 작성한 게시글이 없습니다." />
+            ) : (
+                <NoContentBox text="등록한 친구가 없습니다." />
+            )}
+        </div>
+    );
 }
